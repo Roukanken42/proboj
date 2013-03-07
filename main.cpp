d033826d7abeafaabcd47d40e22cb5648787b80e
Delikvent <training@delikvent.com>
2013-03-07 16:03:10 +0100

    bugfixes

--- main.cpp
+++ main.cpp
@@ -29,11 +29,6 @@ Tah mojTah;
 vector<Bod> d_cka= {{-1,0},{-1,1},{-1,-1},{0,1},{0,-1},{1,0},{1,1},{1,-1}};
 
 
-int sgn(int c);
-void bfs(vector<vector<int> > &nazov, Bod bod);
-void bfs(vector<vector<int> > &nazov, Bod bod, vector <vector<int> > &beware);
-
-
 void inicializuj() {
     debug_init();
 
@@ -57,66 +52,6 @@ void zistiTah() {
     debug("finished");
 }
 
-
-
-void bfs(vector<vector<int> > &nazov, Bod bod) {
-
-    nazov.clear();
-    nazov.resize(mapa.w,vector<int> (mapa.h,100908));
-    queue<Bod> haldicka;
-
-
-    haldicka.push(bod);
-    bod.get(nazov)=0;
-    int pocet = 0;
-
-    while(!haldicka.empty()) {
-        Bod pom = haldicka.front();
-        haldicka.pop();
-
-        for(auto pos: d_cka) {
-            Bod tar = pos + pom;
-
-            if((mapa.zisti(tar.x, tar.y) > 0) && (nazov[tar.x][tar.y] > nazov[pom.x][pom.y] + 1) ) {
-                nazov[tar.x][tar.y] = nazov[pom.x][pom.y]+1;
-                haldicka.push(tar);
-            }
-        }
-    }
-}
-
-void bfs(vector<vector<int> > &nazov, Bod bod, vector<vector<int> > &beware) {
-
-    nazov.clear();
-    nazov.resize(mapa.w,vector<int> (mapa.h,100908));
-    queue<Bod> haldicka;
-
-
-    haldicka.push(bod);
-    bod.get(nazov)=0;
-    int pocet = 0;
-
-    while(!haldicka.empty()) {
-        Bod pom = haldicka.front();
-        haldicka.pop();
-
-        for(auto pos: d_cka) {
-            Bod tar = pos + pom;
-
-            if((mapa.zisti(tar.x, tar.y) > 0) && (nazov[tar.x][tar.y] > nazov[pom.x][pom.y] + 1) ) {
-                if (beware[tar.x][tar.y] == 1) continue;
-                nazov[tar.x][tar.y] = nazov[pom.x][pom.y]+1;
-                haldicka.push(tar);
-            }
-        }
-    }
-}
-
-int sgn(int c) {
-    if (c < 0) return -1;
-    if (c > 0) return 1;
-    return 0;
-}
 
 int main() {
     // v tejto funkcii su vseobecne veci, nemusite ju menit (ale mozte).
